f149f61aecf68741c9558c5ff2869efee31e1bb8
syaofox <syaofox@gmail.com>
2025-09-12 11:29:01 +0800

    更新：启动脚本关闭deepspeed

--- run_webui.sh
+++ run_webui.sh
@@ -4,4 +4,5 @@ cd /home/syaofox/data/dev/index-tts
 
 # 使用 uv 运行 main.py
 source .venv/bin/activate
-python webui.py --verbose --fp16 --use_deepspeed
+# python webui.py --verbose --fp16 --deepspeed
+python webui.py --verbose --fp16
